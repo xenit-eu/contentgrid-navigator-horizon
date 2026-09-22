6241841b4e41699cf9457bf70093d0a60ceae9ae
ranecdev <ranecdev@gmail.com>
2026-09-22 17:00:52 +0200

    remove deprecated filter sidebar

--- packages/ui/src/patterns/filter-sidebar/index.ts
+++
@@ -1,8 +0,0 @@
-export { FilterSidebar } from "./filter-sidebar";
-export type {
-  FilterSidebarProps,
-  SearchFilterProperty,
-  FilterInputKind,
-  SearchOperator,
-  DirectionLabel,
-} from "./filter-sidebar";
